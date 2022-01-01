3aca8cdded059fbceb964c313ebc8d110826d174
shirly <AndreMouche@126.com>
2022-01-01 17:42:21 +0800

    init sql for create table && init cars
    Signed-off-by: shirly <AndreMouche@126.com>

---
+++ docs/data/create_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `cars` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `location_x` int(11) DEFAULT NULL,
+  `location_y` int(11) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) 
+);
+
+create table orders (
+  order_id int(11) not null AUTO_INCREMENT,
+  car_id int(11),
+  from_x int(11),
+  from_y int(11),
+  to_x int(11),
+  to_y int(11),
+  status varchar(20),
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`) 
+);
