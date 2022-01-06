e28d2c5e12b1a04299c332e6d5a6c836da70f745
Cabinfever_B <cabinfeveroier@gmail.com>
2022-01-07 02:11:08 +0800

    demo client
    Signed-off-by: Cabinfever_B <cabinfeveroier@gmail.com>

--- demo_client/main_test.go
+++ demo_client/main_test.go
@@ -7,7 +7,13 @@ import (
 )
 
 func TestMiniDemo(t *testing.T) {
-	getOffWork(pointNum)
+	ids := generateRandomNumber(1, 20, 10)
+	for _, id := range ids {
+		err := letCarGetOffWorkByID(id)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 	goOnWork(20)
 
 	for page := 0; page < 2; page++ {
